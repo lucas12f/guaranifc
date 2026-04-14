a72b29f2e3d879425cf7f035f1df9c845011d114
lucas12f <37491@raphaeldisanto.com.br>
2026-04-14 09:41:55 -0300

    Add files via upload

---
+++ GuraniFC/guaranifc/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+
+  return (
+  <>
+    <main>
+      <section className='foto'></section>
+      <section className='formulario'>
+        <h1>GUARANI FUTEBOL CLUBE</h1>
+        <h2>Sócio Campeão</h2>
+        <h3>E-mail:</h3>
+        <input type="E-mail" placeholder='Digite seu Email...'/>
+        <h3>Senha:</h3>
+        <input type="Senha" placeholder='Digite sua Senha...'/>
+        <button>Logar</button>
+      </section>
+    </main>
+  </>
+  )
+}
