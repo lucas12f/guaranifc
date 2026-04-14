bd8880f8460b506ef55332898fb5ce4cd991fc66
lucas12f <37491@raphaeldisanto.com.br>
2026-04-14 11:41:24 -0300

    Add files via upload

--- GuraniFC/guaranifc/src/App.jsx
+++ GuraniFC/guaranifc/src/App.jsx
@@ -11,11 +11,13 @@ export default function App() {
       <section className='formulario'>
         <h1>GUARANI FUTEBOL CLUBE</h1>
         <h2>Sócio Campeão</h2>
+        <h2 className='login'>Fazer login</h2>
         <h3>E-mail:</h3>
         <input type="E-mail" placeholder='Digite seu Email...'/>
         <h3>Senha:</h3>
         <input type="Senha" placeholder='Digite sua Senha...'/>
         <button>Logar</button>
+        <a href="">Esqueci minha senha</a>
       </section>
     </main>
   </>
